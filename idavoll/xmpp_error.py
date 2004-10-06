ed25d67e229189be2580d030d8cba0648f645a8d
Ralph Meijer <ralphm@ik.nu>
2004-10-06 21:01:13 +0000

    Added two errors
    --HG--
    extra : convert_revision : svn%3Ae60b0d31-0b1b-0410-851a-a02d0c527677/trunk%4020

--- idavoll/xmpp_error.py
+++ idavoll/xmpp_error.py
@@ -4,6 +4,8 @@ conditions = {
 	'bad-request':				{'code': '400', 'type': 'modify'},
 	'not-authorized':			{'code': '401', 'type': 'cancel'},
 	'item-not-found':			{'code': '404', 'type': 'cancel'},
+	'not-acceptable':			{'code': '406', 'type': 'modify'},
+	'conflict':					{'code': '409', 'type': 'cancel'},
 	'internal-server-error':	{'code': '500', 'type': 'wait'},
 	'feature-not-implemented':	{'code': '501', 'type': 'cancel'},
 	'service-unavailable':		{'code': '503', 'type': 'cancel'},
