17ad0a8a43e40a92d124b07ad85fef1e83086fb1
Ralph Meijer <ralphm@ik.nu>
2004-11-07 14:16:37 +0000

    Initial revision
    --HG--
    extra : convert_revision : svn%3Ae60b0d31-0b1b-0410-851a-a02d0c527677/trunk%4070

---
+++ idavoll/disco.py
@@ -0,0 +1,28 @@
+from twisted.xish import domish
+
+NS = 'http://jabber.org/protocol/disco'
+NS_INFO = NS + '#info'
+NS_ITEMS = NS + '#items'
+
+class Feature(domish.Element):
+    def __init__(self, feature):
+        domish.Element.__init__(self, (NS_INFO, 'feature'),
+                                attribs={'var': feature})
+class Identity(domish.Element):
+    def __init__(self, category, type, name = None):
+        domish.Element.__init__(self, (NS_INFO, 'identity'),
+                                attribs={'category': category,
+                                         'type': type})
+        if name:
+            self['name'] = name
+
+class Item(domish.Element):
+    def __init__(self, jid, node = None, name = None):
+        domish.Element.__init__(self, (NS_ITEMS, 'item'),
+                                attribs={'jid': jid})
+        if node:
+            self['node'] = node
+
+        if name:
+            self['name'] = name
+
