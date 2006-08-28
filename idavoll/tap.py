266b5e4d4764a28897a428babc8f87c2cff9de79
Ralph Meijer <ralphm@ik.nu>
2006-08-28 12:44:20 +0000

    Fix spacing errors. Do 0.5.0 release.
    --HG--
    extra : convert_revision : svn%3Ae60b0d31-0b1b-0410-851a-a02d0c527677/trunk%40165

--- idavoll/tap.py
+++ idavoll/tap.py
@@ -7,24 +7,24 @@ from twisted.python import usage
 import idavoll
 
 class Options(usage.Options):
-	optParameters = [
-		('jid', None, 'pubsub'),
-		('secret', None, 'secret'),
-		('rhost', None, '127.0.0.1'),
-		('rport', None, '5347'),
-		('backend', None, 'memory'),
-		('dbuser', None, ''),
-		('dbname', None, 'pubsub'),
-	]
+    optParameters = [
+        ('jid', None, 'pubsub'),
+        ('secret', None, 'secret'),
+        ('rhost', None, '127.0.0.1'),
+        ('rport', None, '5347'),
+        ('backend', None, 'memory'),
+        ('dbuser', None, ''),
+        ('dbname', None, 'pubsub'),
+    ]
 
-	optFlags = [
-		('verbose', 'v', 'Show traffic'),
-		('hide-nodes', None, 'Hide all nodes for disco')
-	]
-	
-	def postOptions(self):
-		if self['backend'] not in ['pgsql', 'memory']:
-			raise usage.UsageError, "Unknown backend!"
+    optFlags = [
+        ('verbose', 'v', 'Show traffic'),
+        ('hide-nodes', None, 'Hide all nodes for disco')
+    ]
+    
+    def postOptions(self):
+        if self['backend'] not in ['pgsql', 'memory']:
+            raise usage.UsageError, "Unknown backend!"
 
 def makeService(config):
-	return idavoll.makeService(config)
+    return idavoll.makeService(config)
