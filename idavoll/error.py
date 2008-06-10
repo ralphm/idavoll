f0c9acaedd9cc36d91758f383a4c703866cde4c7
Ralph Meijer <ralphm@ik.nu>
2008-06-10 11:38:35 +0000

    Adjust spacing to match Twisted's.
    --HG--
    extra : convert_revision : svn%3Ae60b0d31-0b1b-0410-851a-a02d0c527677/trunk%40258

--- idavoll/error.py
+++ idavoll/error.py
@@ -7,40 +7,52 @@ class Error(Exception):
     def __str__(self):
         return self.msg
 
+
 class NodeNotFound(Error):
     pass
 
+
 class NodeExists(Error):
     pass
+
 
 class NotSubscribed(Error):
     """
     Entity is not subscribed to this node.
     """
 
+
 class SubscriptionExists(Error):
     pass
+
 
 class Forbidden(Error):
     pass
 
+
 class ItemForbidden(Error):
     pass
+
 
 class ItemRequired(Error):
     pass
 
+
 class NoInstantNodes(Error):
     pass
+
 
 class InvalidConfigurationOption(Error):
     msg = 'Invalid configuration option'
 
+
 class InvalidConfigurationValue(Error):
     msg = 'Bad configuration value'
 
+
 class NodeNotPersistent(Error):
     pass
+
 
 class NoRootNode(Error):
     pass
