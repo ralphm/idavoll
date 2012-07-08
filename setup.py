e96b471dea91c3c3b8b26c70653c9ec8dcfd52a5
Ralph Meijer <ralphm@ik.nu>
2012-07-07 19:40:11 -0700

    Make sure twisted plugins are installed properly.
    Idavoll provides plugins for twistd. As with all twisted plugins, these
    need to be placed in a directory twisted/plugins so that the plugin
    mechanism can find them. This is not a real package, and when building
    eggs the directory must appear in the egg. However, 'twisted' should not
    occur in the top_level.txt file as this causes warnings when importing
    twisted, and triggers a bug in pip that removes all of twisted when a
    package with a twisted plugin is removed.
    
    This change overrides the way the top_level.txt file is written, by
    explicitly excluding 'twisted' and re-adds `twisted.plugins` to the
    packages being installed.

--- setup.py
+++ setup.py
@@ -15,6 +15,28 @@ install_requires = [
 if sys.version_info < (2, 5):
     install_requires.append('uuid')
 
+# Make sure 'twisted' doesn't appear in top_level.txt
+
+try:
+    from setuptools.command import egg_info
+    egg_info.write_toplevel_names
+except (ImportError, AttributeError):
+    pass
+else:
+    def _top_level_package(name):
+        return name.split('.', 1)[0]
+
+    def _hacked_write_toplevel_names(cmd, basename, filename):
+        pkgs = dict.fromkeys(
+            [_top_level_package(k)
+                for k in cmd.distribution.iter_distribution_names()
+                if _top_level_package(k) != "twisted"
+            ]
+        )
+        cmd.write_file("top-level names", filename, '\n'.join(pkgs) + '\n')
+
+    egg_info.write_toplevel_names = _hacked_write_toplevel_names
+
 setup(name='idavoll',
       version=__version__,
       description='Jabber Publish-Subscribe Service Component',
@@ -25,6 +47,7 @@ setup(name='idavoll',
       packages=[
           'idavoll',
           'idavoll.test',
+          'twisted.plugins',
       ],
       package_data={'twisted.plugins': ['twisted/plugins/idavoll.py',
                                         'twisted/plugins/idavoll_http.py']},
